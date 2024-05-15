4b992d0f768f099a4d41378598e414b0a6111584
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-15 18:41:24 -0300

    handle shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -13,7 +19,26 @@ func main() {
 		w.Write([]byte("Hello World"))
 	})
 
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
+	go func() {
+		fmt.Println("Server is running at http://localhost:3000")
+
+		if err := server.ListenAndServe(); err != nil && http.ErrServerClosed != err {
+			log.Fatalf("Could not listen on %s: %v\n", server.Addr, err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	fmt.Println("Shutting down server...")
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Could not gracefully shutdown server: %v\n", err)
 	}
+
+	fmt.Println("Server stopped")
 }
